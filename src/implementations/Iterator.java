356d3e99d2ebc8d3c518178e52d44a0c830040b0
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-11-24 23:42:36 -0700

    arraylisttests pass

--- src/implementations/Iterator.java
+++ src/implementations/Iterator.java
@@ -1,8 +1,32 @@
 package implementations;
 
 import utilities.IteratorADT;
-
 import java.util.NoSuchElementException;
 
-public class Iterator {
+public class Iterator<E> implements IteratorADT<E> {
+    private final MyArrayList<E> list;
+    private int cursor = 0;
+    private int lastReturned = -1;
+
+    public Iterator(MyArrayList<E> list) {
+        if (list == null) throw new NullPointerException("list is null bro");
+        this.list = list;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return cursor < list.size();
+    }
+
+    @Override
+    public E next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        lastReturned = cursor;
+        cursor++;
+        return list.get(lastReturned);
+    }
+    
+    // TODO
 }
--- src/implementations/MyArrayList.java
+++ src/implementations/MyArrayList.java
@@ -1,203 +1,227 @@
 package implementations;
+
+import java.util.Arrays;
 import utilities.ListADT;
+import implementations.Iterator;
 
-@SuppressWarnings("rawtypes")
-public class MyArrayList<E> implements ListADT {
-	private int size = 0;
-	private static int initSize = 1;
-	E[] elementArr;
-	
-	@SuppressWarnings("unchecked")
-	public MyArrayList() {
-		elementArr = (E[]) new Object[initSize];
-	}
-	
-	@SuppressWarnings("unchecked")
-	private void resize(int newCapacity) {
-		Object[] newArray = new Object[newCapacity];
-		for(int i = 0; i < size(); i++) {
-			newArray[i] = elementArr[i];
-		}
-		elementArr = (E[]) newArray;
-	}
-	
-	@Override
-	public int size() {
-		return size;
-	}
+@SuppressWarnings("unused")
+public class MyArrayList<E> implements ListADT<E> {
+    private int size = 0;
+    private static int INIT_SIZE = 1;
+    private E[] elementArr;
 
-	@Override
-	public void clear() {
-		int arrCapacity = elementArr.length;
-	    for (int i = 0; i < arrCapacity; i++) {
-	    	elementArr[i] = null;
-	    }
-	    size = 0;
-	}
+    @SuppressWarnings("unchecked")
+    public MyArrayList() {
+        elementArr = (E[]) new Object[INIT_SIZE];
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public boolean add(int index, Object toAdd) throws NullPointerException, IndexOutOfBoundsException {
-		if (index > size()) {
-			throw new IndexOutOfBoundsException("index bigger than size");
-		}
-		if (index < 0) {
-			throw new IndexOutOfBoundsException("index less than zero");
-		}
-		if (toAdd == null) {
-			throw new NullPointerException("toAdd is null");
-		}
+    @SuppressWarnings("unchecked")
+    private void resize(int newCapacity) {
+        Object[] newArray = new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = elementArr[i];
+        }
+        elementArr = (E[]) newArray;
+    }
 
-		int capacity = elementArr.length;
-		if (size == capacity) {
-			resize(capacity * 2);
-		}
+    @Override
+    public int size() {
+        return size;
+    }
 
-		// shift??
-		for (int i = size - 1; i >= index; i--) {
-			elementArr[i + 1] = elementArr[i];
-		}
-		elementArr[index] = (E) toAdd;
-		size++;
-		return true;
-	}
+    @Override
+    public void clear() {
+        int arrCapacity = elementArr.length;
+        for (int i = 0; i < arrCapacity; i++) {
+            elementArr[i] = null;
+        }
+        size = 0;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public boolean add(Object toAdd) throws NullPointerException {
-		if (toAdd == null) {
-			throw new NullPointerException("toAdd is null");
-		}
-		int capacity = elementArr.length;
-		if (size == capacity) {
-			resize(Math.max(1, capacity * 2));
-		}
-		elementArr[size++] = (E) toAdd;
-		return true;
-	}
+    @Override
+    public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
+        if (index > size) {
+            throw new IndexOutOfBoundsException("index biger than size");
+        }
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("index less than zero");
+        }
+        if (toAdd == null) {
+            throw new NullPointerException("toAdd is null bro");
+        }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public boolean addAll(ListADT toAdd) throws NullPointerException {
-		if (toAdd == null) {
-			throw new NullPointerException("toAdd is null");
-		}
-		int addSize = toAdd.size();
-		if (addSize == 0) {
-			return false;
-		}
-		// If the source is this list, copy elements first to avoid concurrent modification
-		Object[] toAddArr = toAdd == this ? this.toArray() : toAdd.toArray();
-		// check for null elements in provided collection
-		for (int i = 0; i < toAddArr.length; i++) {
-			if (toAddArr[i] == null) {
-				throw new NullPointerException("toAdd contains null element at index " + i);
-			}
-		}
-		// ensure capacity in bulk
-		int required = size + addSize;
-		int newCap = elementArr.length;
-		if (newCap == 0) newCap = 1;
-		while (newCap < required) newCap *= 2;
-		if (newCap != elementArr.length) resize(newCap);
-		// append elements
-		for (int i = 0; i < toAddArr.length; i++) {
-			elementArr[size++] = (E) toAddArr[i];
-		}
-		return true;
-	}
+        int capacity = elementArr.length;
+        if (size == capacity) {
+            resize(Math.max(1, capacity * 2));
+        }
 
-	@Override
-	public Object get(int index) throws IndexOutOfBoundsException {
-		if (index < 0 || index >= size) {
-			throw new IndexOutOfBoundsException("index out of bounds");
-		}
-		return elementArr[index];
-	}
+        // shift right
+        for (int i = size - 1; i >= index; i--) {
+            elementArr[i + 1] = elementArr[i];
+        }
+        elementArr[index] = toAdd;
+        size++;
+        return true;
+    }
 
-	@Override
-	public Object remove(int index) throws IndexOutOfBoundsException {
-		if (index < 0 || index >= size) {
-			throw new IndexOutOfBoundsException("index out of bounds");
-		}
-		Object removed = elementArr[index];
-		// shift left
-		for (int i = index; i < size - 1; i++) {
-			elementArr[i] = elementArr[i + 1];
-		}
-		elementArr[size - 1] = null;
-		size--;
-		return removed;
-	}
+    @Override
+    public boolean add(E toAdd) throws NullPointerException {
+        if (toAdd == null) {
+            throw new NullPointerException("toAdd is null bro");
+        }
+        int capacity = elementArr.length;
+        if (size == capacity) {
+            resize(Math.max(1, capacity * 2));
+        }
+        elementArr[size++] = toAdd;
+        return true;
+    }
 
-	@Override
-	public Object remove(Object toRemove) throws NullPointerException {
-		if (toRemove == null) {
-			throw new NullPointerException("toRemove is null");
-		}
-		for (int i = 0; i < size; i++) {
-			if (toRemove.equals(elementArr[i])) {
-				return remove(i);
-			}
-		}
-		return null;
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean addAll(ListADT<? extends E> toAdd) throws NullPointerException {
+        if (toAdd == null) {
+            throw new NullPointerException("toAdd is null bro");
+        }
+        int addSize = toAdd.size();
+        if (addSize == 0) {
+            return false;
+        }
+        
+        Object[] toAddArr;
+        if (toAdd == this) {
+            toAddArr = this.toArray();
+        } else {
+            toAddArr = toAdd.toArray();
+        }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public Object set(int index, Object toChange) throws NullPointerException, IndexOutOfBoundsException {
-		if (toChange == null) {
-			throw new NullPointerException("toChange is null");
-		}
-		if (index < 0 || index >= size) {
-			throw new IndexOutOfBoundsException("index out of bounds");
-		}
-		Object old = elementArr[index];
-		elementArr[index] = (E) toChange;
-		return old;
-	}
 
-	@Override
-	public boolean isEmpty() {
-		return size == 0;
-	}
+        // checks for null elements in the thing
+        for (int i = 0; i < toAddArr.length; i++) {
+            if (toAddArr[i] == null) {
+                throw new NullPointerException("toAdd null element at " + i);
+            }
+        }
 
-	@Override
-	public boolean contains(Object toFind) throws NullPointerException {
-		if (toFind == null) {
-			throw new NullPointerException("toFind is null");
-		}
-		for (int i = 0; i < size; i++) {
-			if (toFind.equals(elementArr[i])) {
-				return true;
-			}
-		}
-		return false;
-	}
+        int required = size + addSize;
+        int newCap = elementArr.length;
+        
+        if (newCap == 0) {
+        	newCap = 1;
+        }
+        while (newCap < required) {
+        	newCap *= 2;
+        }
+        if (newCap != elementArr.length) {
+        	resize(newCap);
+        }
 
-	@Override
-	public Object[] toArray(Object[] toHold) throws NullPointerException {
-		if (toHold == null) {
-			throw new NullPointerException("toHold is null");
-		}
-		if (toHold.length < size) {
-			Object[] ret = new Object[size];
-			for (int i = 0; i < size; i++) ret[i] = elementArr[i];
-			return ret;
-		} else {
-			for (int i = 0; i < size; i++) toHold[i] = elementArr[i];
-			if (toHold.length > size) toHold[size] = null;
-			return toHold;
-		}
-	}
+        // append elements
+        for (int i = 0; i < toAddArr.length; i++) {
+            elementArr[size++] = (E) toAddArr[i];
+        }
+        return true;
+    }
 
-	@Override
-	public Object[] toArray() {
-		Object[] ret = new Object[size];
-		for (int i = 0; i < size; i++) {
-			ret[i] = elementArr[i];
-		}
-		return ret;
-	}
+    @Override
+    public E get(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index out of bounds");
+        }
+        return elementArr[index];
+    }
 
+    @Override
+    public E remove(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index out of bounds");
+        }
+        E removed = elementArr[index];
+        // shift left
+        for (int i = index; i < size - 1; i++) {
+            elementArr[i] = elementArr[i + 1];
+        }
+        elementArr[size - 1] = null;
+        size--;
+        return removed;
+    }
+
+    @Override
+    public E remove(E toRemove) throws NullPointerException {
+        if (toRemove == null) {
+            throw new NullPointerException("toRemove is null");
+        }
+        for (int i = 0; i < size; i++) {
+            if (toRemove.equals(elementArr[i])) {
+                return remove(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException {
+        if (toChange == null) {
+            throw new NullPointerException("toChange is null");
+        }
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index out of bounds");
+        }
+        E old = elementArr[index];
+        elementArr[index] = toChange;
+        return old;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(E toFind) throws NullPointerException {
+        if (toFind == null) {
+            throw new NullPointerException("toFind is null");
+        }
+        for (int i = 0; i < size; i++) {
+            if (toFind.equals(elementArr[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public E[] toArray(E[] toHold) throws NullPointerException {
+        if (toHold == null) {
+            throw new NullPointerException("toHold is null");
+        }
+
+        if (toHold.length < size) {
+        	// TODO we allowed to use copyOf?
+            return (E[]) Arrays.copyOf(elementArr, size, toHold.getClass());
+        }
+
+        // TODO we allowed to use System.arraycopy?
+        System.arraycopy(elementArr, 0, toHold, 0, size);
+
+        if (toHold.length > size) {
+            toHold[size] = null;
+        }
+        return toHold;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] ret = new Object[size];
+        for (int i = 0; i < size; i++) {
+            ret[i] = elementArr[i];
+        }
+        return ret;
+    }
+
+    @Override
+    public implementations.Iterator<E> iterator() {
+        return new Iterator<>(this);
+    }
 }
