62f94ce1fc03b9c4e7ad95f727e6e7a1c01ab762
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-11-26 13:22:04 -0700

    javadoc regen

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"appDomain"},{"l":"exceptions"},{"l":"implementations"},{"l":"utilities"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"appDomain"},{"l":"exceptions"},{"l":"implementations"}];updateSearchResults();
